e4993721f43269b2f5e38bbe649da286ee3e6eb9
Rimurutempest187 <karangmerry@gmail.com>
2026-02-04 13:25:35 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from db import init_db
 from handlers import register_handlers
 from keep_alive import keep_alive
 from config import BOT_TOKEN
+from handlers.admin import start_auto_backup
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s | %(levelname)s | %(message)s")
 logger = logging.getLogger(__name__)
@@ -17,6 +18,11 @@ def main():
 
     register_handlers(app)
 
+# save OWNER_ID in bot_data for notification
+    app.bot_data["OWNER_ID"] = OWNER_ID
+
+# start auto daily backup
+    start_auto_backup(app)
     keep_alive()  # optional small web server
 
     logger.info("Bot started")
